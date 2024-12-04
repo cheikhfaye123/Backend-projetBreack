2f294ca264d053dfd3e861111c7780370f8df127
cheikhfaye123 <baboudieye58@gmail.com>
2024-12-04 21:30:08 +0100

    Mise a jour

--- index.js
+++ index.js
@@ -22,7 +22,14 @@ app.use('/api/posts', postRoutes);
 app.use(notFound)
 app.use(errorHandler)
 
-
 connect(process.env.MONGO_URI)
-    .then(app.listen(process.env.PORT || 5000, () => console.log(`Server running on port ${process.env.PORT}`)))
-    .catch(error => console.log(error))
+    .then(() => {
+        console.log('Connexion MongoDB réussie');
+        app.listen(process.env.PORT || 5000, () => {
+            console.log(`Server running on port ${process.env.PORT || 5000}`)
+        });
+    })
+    .catch(error => {
+        console.error('Erreur de connexion MongoDB:', error);
+        process.exit(1);
+    });
