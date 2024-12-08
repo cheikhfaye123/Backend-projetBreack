ea890683bd942f3b5b3fe2c2bba0a7ff3b1c489e
cheikhfaye123 <baboudieye58@gmail.com>
2024-12-08 23:24:28 +0100

    New upload

--- index.js
+++ index.js
@@ -4,12 +4,14 @@ const { connect } = require("mongoose")
 const cors = require('cors')
 const upload = require('express-fileupload')
 const path = require('path');
+const fs = require('fs');
 
 const postRoutes = require('./routes/postRoutes');
 const userRoutes = require('./routes/userRoutes')
 const { notFound, errorHandler } = require('./middleware/errorMiddleware')
 
 const app = express();
+
 app.use(express.json({ extended: true }))
 app.use(express.urlencoded({ extended: true }))
 app.use(cors({
@@ -18,16 +20,28 @@ app.use(cors({
         'https://jocular-cat-aea461.netlify.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    // allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     next();
 });
+
+const uploadsDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadsDir)) {
+    fs.mkdirSync(uploadsDir, { recursive: true });
+}
+
 app.use(upload())
-app.use('/uploads', express.static(__dirname + '/uploads'))
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+
+app.use('/uploads', (req, res, next) => {
+    console.log('Accès fichier:', req.url);
+    console.log('Chemin complet:', path.join(__dirname, 'uploads', req.url));
+    next();
+});
 
 app.use('/api/users', userRoutes);
 app.use('/api/posts', postRoutes);
