5f24d04b64ee85d4ec414d841bba6cd8fbe3784c
cheikhfaye123 <baboudieye58@gmail.com>
2024-12-08 19:28:32 +0100

    mise a jour

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors({
         'https://jocular-cat-aea461.netlify.app'
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
+    // allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
 }));
 app.use((req, res, next) => {
